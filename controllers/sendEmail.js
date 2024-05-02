c497e71df8b01fd2e7e43cea9c86f8be62008ed4
benconsult <bendayo38@gmail.com>
2024-05-02 19:08:58 +0100

    sending email with sendgrid

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,7 +1,8 @@
 
 const nodemailer = require('nodemailer')
+const sgMail = require('@sendgrid/mail')
 
-const sendEmail = async (req,res)=>{
+const sendEmailEthereal = async (req,res)=>{
     //create a test account
     let testAccount = await nodemailer.createTestAccount();
     //ethereal 
@@ -14,7 +15,7 @@ const sendEmail = async (req,res)=>{
         }
     });
 
-    //from nodemail
+    //from nodemail site
     const info = await transporter.sendMail({
         from: '"BenJJy Node Course" <beckyarems@gmail.com>', // sender address
         to: "bendayo38@gmail.com, admin@3171fd.onmicrosoft.com", // list of receivers
@@ -24,7 +25,22 @@ const sendEmail = async (req,res)=>{
       });
 
 
-    res.json(info);
+    //res.json(info);
+}
+
+//sendgrid integration
+const sendEmail = async (req,res)=>{
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: '',
+        from: '', //verified sender on sendgrid
+        subject: '',
+        text: '',
+        html: '' ,// in place of text
+    };
+    const info = await sgMail.send(msg);
+  res.send(info);
+  
 }
 
 module.exports = sendEmail;
